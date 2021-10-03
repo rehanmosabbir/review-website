63f31223f4ee652bee70b2a104f0e1e44c61c7ea
rehanmosabbir <rehanmosabbir@gmail.com>
2021-10-04 00:41:16 +0600

    set up router and install required packages

---
+++ src/serviceData.js
@@ -0,0 +1,62 @@
+const serviceData = [
+  {
+    id: 1,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 2,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 3,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 4,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 5,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 6,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 7,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 8,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 9,
+    image: "",
+    service: "",
+    amount: "",
+  },
+  {
+    id: 10,
+    image: "",
+    service: "",
+    amount: "",
+  },
+];
